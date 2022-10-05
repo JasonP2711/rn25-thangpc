e0a4eef1b12fbaa3e70896b009e95ac6ace6b88f
caothang <cthang2711@gmail.com>
2022-10-05 21:10:57 +0700

    bai thuc hanh tren lop

---
+++ caothang/src/App.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+      The pulput Rock
+      </header>
+      <aside className="section_1">
+        <ul>
+          <li className="my_list">The Drive</li>
+          <li className="my_list">The Walk</li>
+          <li className="my_list">The Return</li>
+          <li className="my_list">The end</li>
+        </ul>
+      </aside>
+      <main>
+        <h2>The Walk</h2>
+    <p>The walk to the pulput Rock will take you approximately two hour, give or take an hour depending on the weather conditions and your physical</p>
+    <image ></image>
+      </main>
+      
+      <aside className="section_2">
+        <ul>
+          <li className="my_list"></li>
+          <li className="my_list"></li>
+          <li className="my_list"></li>
+          <li className="my_list"></li>
+          <li></li>
+        </ul>
+      </aside>
+    </>
+  );
+}
+
+export default App;
