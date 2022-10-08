14a6eb75928cbdc4d46dc4955a21ad2d6f2da7b8
caothang <cthang2711@gmail.com>
2022-10-09 00:01:23 +0700

    homework1

--- caothang/src/App.tsx
+++ caothang/src/App.tsx
@@ -5,32 +5,38 @@ import "./App.css";
 function App() {
   return (
     <>
-      <header>
-      The pulput Rock
-      </header>
-      <aside className="section_1">
-        <ul>
-          <li className="my_list">The Drive</li>
-          <li className="my_list">The Walk</li>
-          <li className="my_list">The Return</li>
-          <li className="my_list">The end</li>
-        </ul>
-      </aside>
-      <main>
-        <h2>The Walk</h2>
-    <p>The walk to the pulput Rock will take you approximately two hour, give or take an hour depending on the weather conditions and your physical</p>
-    <image ></image>
-      </main>
       
-      <aside className="section_2">
+     <div className="header">The Pulpit Rock</div>
+     <div className="listContent">
+      <div className="left">
         <ul>
-          <li className="my_list"></li>
-          <li className="my_list"></li>
-          <li className="my_list"></li>
-          <li className="my_list"></li>
-          <li></li>
+          <li>The Driver</li>
+          <li>The Walk</li>
+          <li>The Return</li>
+          <li>The End</li>
         </ul>
-      </aside>
+      </div>
+      <div className="content"><h2>The Walk</h2>
+      <p>The walk to the Pulpit Rock will tack you approximately two hour, give of take an hour depending on the weather conditions and your physical shape </p>
+      <img src="https://img0.oastatic.com/img2/35906371/max/preikestolen-pulpit-rock-.jpg"></img>
+      </div>
+      <div className="right">
+        <ul>
+          <li><h2>What</h2>
+    <p>Pulpit Rock is a part of a mountain that look like a pulpit</p>
+          </li>
+          <li><h2>Where</h2>
+          <p>Pulpit Rock is a Norway</p>
+          </li>
+          <li><h2>Price</h2>
+          <p>The walk is free</p>
+          </li>
+          
+        </ul>
+      </div>
+      
+     </div>
+     <div className="footer"></div>
     </>
   );
 }
